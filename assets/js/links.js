48d3d4b2ac8b17ebad216216f2864379ae791141
S Bester <sarahleebester@gmail.com>
2023-08-19 20:28:40 +0100

    Re-test JavaScript fix for join.html section links

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,15 +1,15 @@
 function goToSection () {
-    const groups = document.getElementById('groups-link');
-    const meetUps = document.getElementById('meetups-link');
-    const aboutUs = document.getElementById('about-us-link');
-    groups.addEventListener('click', function () {
-      window.location.hash = 'index.html#groups'
-    })
-    meetUps.addEventListener('click', function () {
-      window.location.hash = 'index.html#meetups'
-    })
-    aboutUs.addEventListener('click', function () {
-      window.location.hash = 'index.html#about-us'
-    })
-  }
-  goToSection();
+  const groups = document.getElementById('groups');
+  const meetUps = document.getElementById('meetups');
+  const aboutUs = document.getElementById('about-us');
+  groups.addEventListener('click', function () {
+    window.location = 'index.html#groups';
+  })
+  meetUps.addEventListener('click', function () {
+    window.location = 'index.html#meetups';
+  })
+  aboutUs.addEventListener('click', function () {
+    window.location = 'index.html#about-us';
+  })
+}
+goToSection();
